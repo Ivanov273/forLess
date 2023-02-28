b561323dc11dbbe1fdaf49b6a2c7a7150bd9028f
Ivanov273 <Ivanov.Michail.2018@gmail.com>
2023-02-28 10:19:08 +0300

    start 28

---
+++ index.js
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+    <title>Первый сайт</title>
+<link rel="stylesheet" href="style.css">
+    <!--[if IE]>
+     <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <style>
+        .round {
+        border-radius: 10000000000px;
+
+    }
+
+    </style>
+    <style>
+        .fig {
+        text-align: center;
+    }
+    </style>
+</head>
+<body class="bodyy" bgcolor="blue">
+<h1></h1>
+<form action="/examples/php-scripts/coords.php" method="get">
+    <p><input type="image" src="/examples/images/figures.png" name="coords" alt="Пример графической кнопки" style="background: #ededed;"></p>
+</form>
+<p class="center-img"></p>
+<script >
+    function showMessage() {
+    alert( 'Всем привет!' );
+    console.log('dadadad')
+}
+
+
+</script>
+<p> class="fig">
+    <img onclick="showMessage()" align="middle" src="сапаг.jpg" "alt="" class="round"></h1p>
+
+</body>
+
+
+
+
